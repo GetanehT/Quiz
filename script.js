bbaf7abc6607ba2e1ec94bb49a0d744962d59386
Getaneh Biratu <mrgetaneh@gmail.com>
2024-06-26 00:46:46 +0000

    adding js

---
+++ script.js
@@ -0,0 +1,61 @@
+var questions = [
+
+  {  question: "which is the wealthest country?",
+    answers: {
+      { text: "England", correct: true },
+      { text: "Germany", correct: false},
+      { text: "USA", correct: false},
+      { text: "Australia", correct: false},
+    }
+  },
+    {  
+      question: "which is the wealthest country?",
+      answers: {
+        { text: "England", correct: false },
+        { text: "Germany", correct: false},
+        { text: "USA", correct: false},
+        { text: "Australia", correct: false},
+      }
+    }, 
+
+      {  question: "which is the wealthest country?",
+        answers: {
+          { text: "England", correct: false },
+          { text: "Germany", correct: false},
+          { text: "USA", correct: false},
+          { text: "Australia", correct: false},
+        }
+      },
+      
+
+          {  question: "which is the wealthest country?",
+            answers: {
+              { text: "England", correct: false },
+              { text: "Germany", correct: false},
+              { text: "USA", correct: false},
+              { text: "Australia", correct: false},
+            }
+          }
+        ]
+        
+        
+
+
+function Function1()
+{
+  document.getElementById("question").innerHTML = "Hello Getaneh 1";
+}
+
+function Function2()
+{
+  document.getElementById("ask2").innerHTML = "Hello Getaneh number 2";
+}
+function Function3()
+{
+  document.getElementById("ask3").innerHTML = "Hello Getaneh number 3";
+}
+
+function Function4()
+{
+  document.getElementById("ask4").innerHTML = "Hello Getaneh number 4";
+}
